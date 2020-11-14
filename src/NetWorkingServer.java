6ebf2952d71407d2e4fa8bb60013aa08d2c8a567
e8251625 <18251625@life.hkbu.edu.hk>
2020-11-14 16:17:34 +0800

    hyuyfyt

---
+++ src/NetWorkingServer.java
@@ -0,0 +1,10 @@
+
+public class NetWorkingServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("000");
+		System.out.print("000");
+	}
+
+}
